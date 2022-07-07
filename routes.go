cb0d5ce6b369acd5c4d7691c9516d03c7b34c004
fangchoice <625486143@qq.com>
2022-07-07 10:29:04 +0800

    Add files via upload

---
+++ routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"gin-learn/controller"
+	"gin-learn/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 路由文件
+func CollectRoute(r *gin.Engine) *gin.Engine {
+	r.POST("/api/auth/register", controller.Register)
+	r.POST("/api/auth/login", controller.Login)
+	r.GET("/api/auth/info", middleware.AuthMiddleware(), controller.Info)
+
+	return r
+}
