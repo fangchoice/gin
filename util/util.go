e5d0d60442f89114b82413288655ff3c17a1f32d
fangchoice <625486143@qq.com>
2022-07-07 10:27:32 +0800

    Add files via upload

---
+++ util/util.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandomString(n int) string {
+	// 随机生成一个字符串
+	var letters = []byte("asdfghjklzxcvbnmqwertyuiopASDFGHJKLZXCVBNMQWERTYUIOP")
+	result := make([]byte, n)
+
+	rand.Seed(time.Now().Unix())
+	for i := range result {
+		result[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(result)
+}
